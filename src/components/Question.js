14db51dd37fc2a38899a22ae0d8f8f598734670f
Sasha <georgefbrockman@gmail.com>
2024-01-14 22:32:03 -0600

    finishes lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const timerID = setTimeout(() => {
+      if(timeRemaining > 1){
+        setTimeRemaining((timeRemaining) => timeRemaining -1)
+      }else{
+        setTimeRemaining(10);
+        onAnswered(false);
+      }
+    }, 1000);
+
+    return function cleanup(){
+      clearTimeout(timerID);
+    }
+  }, [timeRemaining])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
